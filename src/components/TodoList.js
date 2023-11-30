1794dce4964571f8dc6fbf20b0e7c5ae39c5fba5
Jayant pawar <pawarjayant80@gmail.com>
2023-11-30 19:30:43 +0530

    change in TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -21,7 +21,7 @@ useEffect(() => {
   const fetchData = async () => {
     try {
       const response = await axios.get('http://localhost:8000/todos');
-      dispatch({ type: 'ADD_TODO', payload: response.data.map((item=>(item.id))) });
+      dispatch({ type: 'ADD_TODO', payload: response.data });
       console.log(response)
     } catch (error) {
       console.error("Error fetching todos from server:", error);
